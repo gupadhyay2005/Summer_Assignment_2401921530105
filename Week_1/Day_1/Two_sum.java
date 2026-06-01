01a15369f853cd70684bfb38a530d6347c178fc6
gupadhyay2005 <gupadhyay2005@gmail.com>
2026-06-01 23:01:55 +0530

    Update Two_sum.java

--- Week_1/Day_1/Two_sum.java
+++ Week_1/Day_1/Two_sum.java
@@ -1,1 +1,12 @@
-
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+for (int i=0; i<nums.length ; i++){
+ for (int j =i +1; j<nums.length; j++){
+        if((nums[i] +nums[j] ==target)){
+            System.out.println("Indices: [" + i + ", " + j + "]");
+                    return new int[] { i, j };
+        }
+    }
+}
+ return new int[] {};   }
+}
