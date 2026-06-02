aee79d70cf56b93cf040008a53e91972c12cfc70
gupadhyay2005 <gupadhyay2005@gmail.com>
2026-06-02 22:59:12 +0530

    Update Maximum_Subarray.java

--- Week_1/Day_2/Maximum_Subarray.java
+++ Week_1/Day_2/Maximum_Subarray.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    public int maxSubArray(int[] nums) {
+   int currentsum =0;
+   int maxsum =Integer.MIN_VALUE ;
+   for(int i =0 ; i<nums.length ; i++){
+    currentsum = currentsum + nums[i];
+       maxsum =Math.max(maxsum , currentsum);
+    if(currentsum < 0){
+        currentsum =0;
+    }
+ }
+ return maxsum ;
+    }
+}
