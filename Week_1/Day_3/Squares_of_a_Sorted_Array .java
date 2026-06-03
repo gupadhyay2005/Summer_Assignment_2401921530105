84acfaef3a3e1dd68cf07adad69094ce133669b9
gupadhyay2005 <gupadhyay2005@gmail.com>
2026-06-03 23:33:11 +0530

    Update Squares_of_a_Sorted_Array .java

--- Week_1/Day_3/Squares_of_a_Sorted_Array .java
+++ Week_1/Day_3/Squares_of_a_Sorted_Array .java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int len = nums.length;
+        int[] ans = new int[len];
+        int start = 0, end = len - 1;
+        for (int i = len - 1; i >= 0; i--) {
+            if (Math.abs(nums[start]) > Math.abs(nums[end])) {
+                ans[i] = nums[start] * nums[start];
+                start++;
+            } else {
+                ans[i] = nums[end] * nums[end];
+                end--;
+            }
+        }
+        return ans;
+    }
+}
