6b467d69e91b189e55bfe7c0fd699764625a9c05
gupadhyay2005 <gupadhyay2005@gmail.com>
2026-06-04 16:56:45 +0530

    Update Spiral_Matrix.java

--- Week_1/Day_4/Spiral_Matrix.java
+++ Week_1/Day_4/Spiral_Matrix.java
@@ -1,1 +1,41 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
 
+        int startrow = 0;
+        int startcol = 0;
+        int endrow = matrix.length - 1;
+        int endcol = matrix[0].length - 1;
+
+        List<Integer> list = new ArrayList<>();
+
+        while (startrow <= endrow && startcol <= endcol) {
+ for (int j = startcol; j <= endcol; j++) {
+                list.add(matrix[startrow][j]);
+            }
+
+            
+            for (int i = startrow + 1; i <= endrow; i++) {
+                list.add(matrix[i][endcol]);
+            }
+
+            
+            for (int j = endcol - 1; j >= startcol; j--) {
+                if (startrow == endrow) break;
+                list.add(matrix[endrow][j]);
+            }
+
+      
+            for (int i = endrow - 1; i > startrow; i--) {
+                if (startcol == endcol) break;
+                list.add(matrix[i][startcol]);
+            }
+
+            startrow++;
+            startcol++;
+            endrow--;
+            endcol--;
+        }
+
+        return list;
+    }
+}
