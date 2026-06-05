6fc4d68c0b07f74487cf3ef816ed911c3e6bffd1
gupadhyay2005 <gupadhyay2005@gmail.com>
2026-06-05 23:53:04 +0530

    Update Longest_Common_Prefix.java

--- Week_1/Day_5/Longest_Common_Prefix.java
+++ Week_1/Day_5/Longest_Common_Prefix.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        int n = strs.length;
+        int m = strs[0].length();
+        StringBuilder ans = new StringBuilder();
+        for( int i = 0 ; i <m ; i++){
+             char ch = strs[0].charAt(i);
+           for( int j = 0 ; j< n ; j++){
+            if(i>= strs[j].length()   || ch != strs[j].charAt(i) ){
+                return ans.toString();
+            }
+             
+           }
+           ans.append(ch);
+        }      
+        return ans.toString();
+        }
+}
