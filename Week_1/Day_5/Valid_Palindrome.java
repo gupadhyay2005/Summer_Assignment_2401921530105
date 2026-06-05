a76a101dd239ac3da41c4303c4422c3a04e61144
gupadhyay2005 <gupadhyay2005@gmail.com>
2026-06-05 22:32:32 +0530

    Update Valid_Palindrome.java

--- Week_1/Day_5/Valid_Palindrome.java
+++ Week_1/Day_5/Valid_Palindrome.java
@@ -1,1 +1,29 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String g = "";
+        for( int i = 0 ; i<s.length() ; i++){
+             char ch = s.charAt(i);
 
+if (!(ch >= 'a' && ch <= 'z') &&
+    !(ch >= 'A' && ch <= 'Z') &&
+    !(ch >= '0' && ch <= '9')) {
+    continue;
+}
+
+g += ch;
+g =g.toLowerCase();
+        }
+        int i = 0;
+        int j = g.length() - 1;
+        while(i<j){
+            if(g.charAt(i)!= g.charAt(j)){
+                return false;
+                
+            }
+            i++;
+                j--;
+        }
+        return true;
+        
+    }
+}
