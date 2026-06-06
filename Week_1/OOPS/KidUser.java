d766d12c7aefa73ece7b2292ab84988ea57701db
gupadhyay2005 <gupadhyay2005@gmail.com>
2026-06-06 23:30:59 +0530

    Update KidUser.java

--- Week_1/OOPS/KidUser.java
+++ Week_1/OOPS/KidUser.java
@@ -1,15 +1,9 @@
-class KidUser implements LibraryUser {
-
+class KidUsers implements LibraryUser {
     int age;
     String bookType;
 
-    KidUser(int age, String bookType) {
-        this.age = age;
-        this.bookType = bookType;
-    }
-
+    @Override
     public void registerAccount() {
-
         if (age < 12) {
             System.out.println("You have successfully registered under a Kids Account");
         } else {
@@ -17,9 +11,9 @@ class KidUser implements LibraryUser {
         }
     }
 
+    @Override
     public void requestBook() {
-
-        if (bookType.equals("Kids")) {
+        if (bookType.equalsIgnoreCase("Kids")) {
             System.out.println("Book Issued successfully, please return the book within 10 days");
         } else {
             System.out.println("Oops, you are allowed to take only kids books");
