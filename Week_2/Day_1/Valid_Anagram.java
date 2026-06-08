cced46ad23b7b031d3c512f452fbafb6b92a5ff3
gupadhyay2005 <gupadhyay2005@gmail.com>
2026-06-08 23:33:15 +0530

    Update Valid_Anagram.java

--- Week_2/Day_1/Valid_Anagram.java
+++ Week_2/Day_1/Valid_Anagram.java
@@ -1,1 +1,29 @@
-
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        HashMap<Character, Integer> map = new HashMap<>();
+                int n = s.length();
+                for(int i = 0; i<n ; i++){
+                    char c = s.charAt(i);
+                    if(map.containsKey(c)){
+                        map.put(c, map.get(c)+1);
+                    }else{
+                        map.put(c, 1);
+                    }
+                }
+                for( int i = 0 ; i< t.length() ; i++){
+                  char c = t.charAt(i);
+                  if(!map.containsKey(c)){
+                    return false;
+                  }else if(map.get(c) == 1){
+                    map.remove(c);
+                  }else{
+                    map.put(c , map.get(c)-1);
+                  }
+                }
+                return map.isEmpty();
+        
+    }
+}
