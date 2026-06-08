8f2ce519b32a598c8e872aa9f55a7bc26cabb634
gupadhyay2005 <gupadhyay2005@gmail.com>
2026-06-08 23:37:37 +0530

    Update Ransom_Note.java

--- Week_2/Day_1/Ransom_Note.java
+++ Week_2/Day_1/Ransom_Note.java
@@ -1,1 +1,21 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] freq = new int[26];
 
+        for(int i = 0; i < magazine.length(); i++) {
+            freq[magazine.charAt(i) - 'a']++;
+        }
+
+        for(int i = 0; i < ransomNote.length(); i++) {
+            int idx = ransomNote.charAt(i) - 'a';
+
+            if(freq[idx] == 0) {
+                return false;
+            }
+
+            freq[idx]--;
+        }
+
+        return true;
+    }
+}
