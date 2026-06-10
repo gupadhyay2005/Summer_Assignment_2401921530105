bd63e04450f2dab6aeb585f7cd223f51befc1f7e
gupadhyay2005 <gupadhyay2005@gmail.com>
2026-06-10 23:33:04 +0530

    Update Implement_strStr.java

--- Week_2/Day_3/Implement_strStr.java
+++ Week_2/Day_3/Implement_strStr.java
@@ -1,1 +1,20 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
 
+        for (int start = 0; start <= haystack.length() - needle.length(); start++) {
+
+            int ptr = 0;
+
+            while (ptr < needle.length() &&
+                   haystack.charAt(start + ptr) == needle.charAt(ptr)) {
+                ptr++;
+            }
+
+           a if (ptr == needle.length()) {
+                return start;
+            }
+        }
+
+        return -1;
+    }
+}
