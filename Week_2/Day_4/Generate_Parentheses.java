b4d8e37f642619a4ce1a84619cdaf653bd3b267f
gupadhyay2005 <gupadhyay2005@gmail.com>
2026-06-11 23:31:20 +0530

    Update Generate_Parentheses.java

--- Week_2/Day_4/Generate_Parentheses.java
+++ Week_2/Day_4/Generate_Parentheses.java
@@ -1,1 +1,28 @@
+class Solution {
 
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        solve(n, 0, 0, "", result);
+        return result;
+    }
+
+    void solve(int n, int open, int close,
+               String temp, List<String> result) {
+
+        // ✅ base case
+        if (temp.length() == 2 * n) {
+            result.add(temp);
+            return;
+        }
+
+        // ✅ add '(' if we can
+        if (open < n) {
+            solve(n, open + 1, close, temp + "(", result);
+        }
+
+        // ✅ add ')' if valid
+        if (close < open) {
+            solve(n, open, close + 1, temp + ")", result);
+        }
+    }
+}
