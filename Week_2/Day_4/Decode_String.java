f9f0f515f27f6f4804f5c1a91d823f4ffb6fc8d2
gupadhyay2005 <gupadhyay2005@gmail.com>
2026-06-11 23:23:16 +0530

    Update Decode_String.java

--- Week_2/Day_4/Decode_String.java
+++ Week_2/Day_4/Decode_String.java
@@ -1,1 +1,43 @@
+class Solution {
+    public String decodeString(String s) {
 
+        Stack<Integer> countStack = new Stack<>();
+        Stack<String> stringStack = new Stack<>();
+
+        String currentString = "";
+        int currentNum = 0;
+
+        for (char ch : s.toCharArray()) {
+
+            if (Character.isDigit(ch)) {
+                currentNum = currentNum * 10 + (ch - '0');
+            }
+
+            else if (ch == '[') {
+                countStack.push(currentNum);
+                stringStack.push(currentString);
+
+                currentNum = 0;
+                currentString = "";
+            }
+
+            else if (ch == ']') {
+
+                int repeat = countStack.pop();
+                StringBuilder temp = new StringBuilder(stringStack.pop());
+
+                for (int i = 0; i < repeat; i++) {
+                    temp.append(currentString);
+                }
+
+                currentString = temp.toString();
+            }
+
+            else {
+                currentString += ch;
+            }
+        }
+
+        return currentString;
+    }
+}
