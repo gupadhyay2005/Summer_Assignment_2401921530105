e6da02b937ec0d6038e8a7392ae7fff82e2040c1
gupadhyay2005 <gupadhyay2005@gmail.com>
2026-06-11 21:25:14 +0530

    Update Reverse_Words_in_a_String_III.java

--- Week_2/Day_4/Reverse_Words_in_a_String_III.java
+++ Week_2/Day_4/Reverse_Words_in_a_String_III.java
@@ -1,1 +1,35 @@
-
+class Solution {
+    public String reverseWords(String s) {
+    
+    int n = s.length();
+    String x ="" ;
+    String y ="";
+        for( int i = 0 ; i<n ; i++){
+            char c = s.charAt(i);
+            if(c ==' '){
+                int a= y.length() -1;
+                 
+                while(a>=0){
+                   x+=y.charAt(a);
+                   a--;
+                   
+                }
+                x+=' ';
+                y="";
+            }else{
+            y+=c;
+            }
+      
+        
+    }
+       
+      int   a =y.length() -1;
+                while(a>=0){
+                   x+=y.charAt(a);
+                   a--;
+                   
+                }
+    return x;
+        
+    }
+}
