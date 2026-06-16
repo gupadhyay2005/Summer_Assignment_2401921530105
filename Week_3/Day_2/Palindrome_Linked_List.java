e1db1c845cb9513b05cfcc7c4114be41034ceac2
gupadhyay2005 <gupadhyay2005@gmail.com>
2026-06-16 12:45:24 +0530

    Update Palindrome_Linked_List.java

--- Week_3/Day_2/Palindrome_Linked_List.java
+++ Week_3/Day_2/Palindrome_Linked_List.java
@@ -1,1 +1,39 @@
 
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode mid = middleNode(head);
+         ListNode curr = mid;
+ListNode next;
+ListNode prev = null;
+while(curr!=null){
+    next = curr.next;
+curr.next = prev;
+prev =curr;
+curr = next;
+}
+ListNode right = prev;
+ListNode left = head;
+
+while(right!= null){
+    if(left.val != right.val ){
+        return false;
+    }
+    left  = left.next;
+    right = right.next;
+}
+return true;
+
+        
+    }
+    public   ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;       // move 1 step
+            fast = fast.next.next; // move 2 steps
+        }
+
+        return slow;
+    }
+
+}
