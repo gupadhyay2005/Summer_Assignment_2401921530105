f747ed8feacb7f6223a2d3426a084234a74200f7
gupadhyay2005 <gupadhyay2005@gmail.com>
2026-06-17 06:55:44 +0530

    Update Valid_Parentheses.java

--- Week_3/Day_3/Valid_Parentheses.java
+++ Week_3/Day_3/Valid_Parentheses.java
@@ -1,1 +1,45 @@
+import java.util.*;
 
+class Solution {
+
+    public boolean isValid(String s) {
+
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char c = s.charAt(i);
+
+            // opening brackets
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            }
+
+            // closing brackets
+            else {
+
+                if (st.isEmpty()) {
+                    return false;
+                }
+
+                char top = st.peek();
+
+                if (c == ')' && top != '(') {
+                    return false;
+                }
+
+                if (c == '}' && top != '{') {
+                    return false;
+                }
+
+                if (c == ']' && top != '[') {
+                    return false;
+                }
+
+                st.pop();
+            }
+        }
+
+        return st.isEmpty();
+    }
+}
