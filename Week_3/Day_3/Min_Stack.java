362e27be601063d042dd112386059ca72571926c
gupadhyay2005 <gupadhyay2005@gmail.com>
2026-06-17 07:00:47 +0530

    Update Min_Stack.java

--- Week_3/Day_3/Min_Stack.java
+++ Week_3/Day_3/Min_Stack.java
@@ -1,1 +1,48 @@
+class MinStack {
+    Stack<Integer>  minstack = new Stack<>();
+        Stack<Integer>  s = new Stack<>();
 
+
+    public MinStack() {
+    }
+
+    
+    public void push(int value) {
+       
+    s.push(value);
+    if(minstack.isEmpty() || minstack.peek()>=value){
+        minstack.push(value);
+    }
+        
+    }
+    
+    public void pop() {
+        if(minstack.peek().equals(s.peek())){
+            minstack.pop();
+        }
+        
+        s.pop();
+        
+            }
+    
+    public int top() {
+        return  s.peek();
+        
+    }
+    
+    public int getMin() {
+        return minstack.peek();
+        }
+        
+        
+    
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(value);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
