9218452e189c86bb117f43a167a98e850f4b3198
gupadhyay2005 <gupadhyay2005@gmail.com>
2026-06-18 11:40:41 +0530

    Update Evaluate_Reverse_Polish_Notation.java

--- Week_3/Day_4/Evaluate_Reverse_Polish_Notation.java
+++ Week_3/Day_4/Evaluate_Reverse_Polish_Notation.java
@@ -1,1 +1,51 @@
+class Solution {
 
+    public int evalRPN(String[] tokens) {
+
+        Stack<Integer> s = new Stack<>();
+
+        for(int i = 0; i < tokens.length; i++) {
+
+            String x = tokens[i];
+
+            if(x.equals("+")) {
+
+                int b = s.pop();
+                int a = s.pop();
+
+                s.push(a + b);
+            }
+
+            else if(x.equals("-")) {
+
+                int b = s.pop();
+                int a = s.pop();
+
+                s.push(a - b);
+            }
+
+            else if(x.equals("*")) {
+
+                int b = s.pop();
+                int a = s.pop();
+
+                s.push(a * b);
+            }
+
+            else if(x.equals("/")) {
+
+                int b = s.pop();
+                int a = s.pop();
+
+                s.push(a / b);
+            }
+
+            else {
+
+                s.push(Integer.parseInt(x));
+            }
+        }
+
+        return s.peek();
+    }
+}
