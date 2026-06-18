8293b870788b45b578b74cea8dfd00081b58f862
gupadhyay2005 <gupadhyay2005@gmail.com>
2026-06-18 11:38:38 +0530

    Update Daily_Temperatures.java

--- Week_3/Day_4/Daily_Temperatures.java
+++ Week_3/Day_4/Daily_Temperatures.java
@@ -1,1 +1,24 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        Stack<Integer> s = new Stack<>();
+         int ans[] = new int[n];
+         int k =n-1;
+        for( int  i = n-1 ; i>=0 ; i--){
 
+            while(!s.isEmpty() && temperatures[s.peek()] <= temperatures[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                ans[k--] =0;
+            }else{
+                ans[k--] = s.peek() - i;
+            }
+            s.push(i);
+
+
+        }
+        return ans;
+        
+    }
+}
