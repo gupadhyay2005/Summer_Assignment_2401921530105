f02de93c5aba6770749b6840e45fed34f0463a07
gupadhyay2005 <gupadhyay2005@gmail.com>
2026-06-19 13:20:40 +0530

    Update Implement_Queue_using_Stacks.java

--- Week_3/Day_5/Implement_Queue_using_Stacks.java
+++ Week_3/Day_5/Implement_Queue_using_Stacks.java
@@ -1,1 +1,39 @@
+import java.util.*;
 
+class MyQueue {
+
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
+
+    public MyQueue() {
+
+    }
+
+    public void push(int x) {
+
+        // Move all elements from s1 to s2
+        while (!s1.isEmpty()) {
+            s2.push(s1.pop());
+        }
+
+        // Push new element into s1
+        s1.push(x);
+
+        // Move back all elements from s2 to s1
+        while (!s2.isEmpty()) {
+            s1.push(s2.pop());
+        }
+    }
+
+    public int pop() {
+        return s1.pop();
+    }
+
+    public int peek() {
+        return s1.peek();
+    }
+
+    public boolean empty() {
+        return s1.isEmpty();
+    }
+}
