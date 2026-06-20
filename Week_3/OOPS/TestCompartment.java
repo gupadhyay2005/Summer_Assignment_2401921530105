c6a660c4ac89fb42109f32c9ec99ce5610f716bf
gupadhyay2005 <gupadhyay2005@gmail.com>
2026-06-20 17:16:43 +0530

    Update TestCompartment.java

--- Week_3/OOPS/TestCompartment.java
+++ Week_3/OOPS/TestCompartment.java
@@ -1,1 +1,31 @@
+public class TestCompartment {
 
+    public static void main(String[] args) {
+
+        Compartment arr[] = new Compartment[10];
+
+        for (int i = 0; i < 10; i++) {
+
+            int r = (int)(Math.random() * 4) + 1;
+
+            if (r == 1) {
+                arr[i] = new FirstClass();
+            }
+            else if (r == 2) {
+                arr[i] = new Ladies();
+            }
+            else if (r == 3) {
+                arr[i] = new General();
+            }
+            else {
+                arr[i] = new Luggage();
+            }
+        }
+
+        System.out.println("Compartment Notices:");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(arr[i].notice());
+        }
+    }
+}
