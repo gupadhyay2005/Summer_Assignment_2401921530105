2a997ebaf65008f2a14ab9d345b19abe206160f3
gupadhyay2005 <gupadhyay2005@gmail.com>
2026-06-22 15:41:56 +0530

    Update Same_Tree.java

--- Week_4/Day_1/Same_Tree.java
+++ Week_4/Day_1/Same_Tree.java
@@ -1,1 +1,29 @@
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return checking(p, q);
+    }
 
+    public Boolean checking(TreeNode p, TreeNode q) {
+
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        Boolean x = isSameTree(p.left, q.left);
+        Boolean y = isSameTree(p.right, q.right);
+
+        if (x && y) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
