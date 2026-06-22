f494eb2f9f26b52877f23608bf3e22464fda00b8
gupadhyay2005 <gupadhyay2005@gmail.com>
2026-06-22 15:35:55 +0530

    Update Maximum_Depth_of_Binary_Tree.java

--- Week_4/Day_1/Maximum_Depth_of_Binary_Tree.java
+++ Week_4/Day_1/Maximum_Depth_of_Binary_Tree.java
@@ -1,1 +1,28 @@
 
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        
+    int range = leafnode(root);
+    return range;
+
+        
+        
+    }
+    public int leafnode(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+
+        int left  = leafnode(root.left);
+        int right = leafnode(root.right);
+
+        
+    
+            return (Math.max(left , right)) +1;
+                   
+    }
+}
