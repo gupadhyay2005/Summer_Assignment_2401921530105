2a5aabfeebec461d6505f9de8248482106783851
gupadhyay2005 <gupadhyay2005@gmail.com>
2026-06-23 23:38:47 +0530

    Update  Binary_Tree_Level_Order_Traversal.java

--- Week_4/Day_2/ Binary_Tree_Level_Order_Traversal.java
+++ Week_4/Day_2/ Binary_Tree_Level_Order_Traversal.java
@@ -1,1 +1,80 @@
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        List<List<Integer>>  ans = new  ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+     
+if(root == null){
+    return ans;
+}
+     q.add(root);
+     q.add(null);
+
+
+     while(!q.isEmpty()){
+        TreeNode  a = q.remove();
+
+
+
+        if( a == null){
+            ans.add(temp);
+            if(q.isEmpty()){
+                break;
+            }
+            else{
+                
+           temp= new ArrayList<>();
+           q.add(null); 
+
+
+            }
+
+
+
+        }else{
+           temp.add(a.val);
+           if(a.left!= null){
+            q.add(a.left);
+           }
+           if(a.right!= null){
+            q.add(a.right);
+           }
+
+        }
+
+   }
+   return ans;
+        
+    }
+}
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+  
