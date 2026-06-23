586e174b2e9c60d23ca5b2964bbc9196b9a88354
gupadhyay2005 <gupadhyay2005@gmail.com>
2026-06-23 23:43:23 +0530

    Update Binary_Tree_Zigzag_Level_Order_Traversal.java

--- Week_4/Day_2/Binary_Tree_Zigzag_Level_Order_Traversal.java
+++ Week_4/Day_2/Binary_Tree_Zigzag_Level_Order_Traversal.java
@@ -1,1 +1,63 @@
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
 
+        List<List<Integer>> ans = new ArrayList<>();
+
+        if(root == null){
+            return ans;
+        }
+
+        Queue<TreeNode> q = new LinkedList<>();
+
+        q.add(root);
+        q.add(null);
+
+        List<Integer> temp = new ArrayList<>();
+
+        int k = 0;
+
+        while(!q.isEmpty()){
+
+            TreeNode a = q.remove();
+
+            if(a == null){
+
+                if(k % 2 == 1){
+                  //  Collections.reverse(temp);
+                  List<Integer> rev = new ArrayList<>();
+
+for(int i = temp.size() - 1; i >= 0; i--){
+    rev.add(temp.get(i));
+}
+
+temp =rev;
+                }
+
+                ans.add(temp);
+
+                if(q.isEmpty()){
+                    break;
+                }
+
+                temp = new ArrayList<>();
+                k++;
+
+                q.add(null);
+            }
+            else{
+
+                temp.add(a.val);
+
+                if(a.left != null){
+                    q.add(a.left);
+                }
+
+                if(a.right != null){
+                    q.add(a.right);
+                }
+            }
+        }
+
+        return ans;
+    }
+}
