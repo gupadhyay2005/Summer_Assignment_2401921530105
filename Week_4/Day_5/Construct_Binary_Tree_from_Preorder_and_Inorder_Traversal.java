750914ca98175f7cdc5d447789cea2d34d6cc65d
gupadhyay2005 <gupadhyay2005@gmail.com>
2026-06-26 22:25:41 +0530

    Update Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.java

--- Week_4/Day_5/Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.java
+++ Week_4/Day_5/Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.java
@@ -1,1 +1,31 @@
+class Solution {
 
+    int preIndex = 0;
+    HashMap<Integer, Integer> map = new HashMap<>();
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+
+        return build(preorder, 0, inorder.length - 1);
+    }
+
+    public TreeNode build(int[] preorder, int start, int end) {
+
+        if (start > end) {
+            return null;
+        }
+
+        int rootVal = preorder[preIndex++];
+        TreeNode root = new TreeNode(rootVal);
+
+        int index = map.get(rootVal);
+
+        root.left = build(preorder, start, index - 1);
+        root.right = build(preorder, index + 1, end);
+
+        return root;
+    }
+}
