838f1ed2231bb3aa108e2b5ef4f0019603153591
gupadhyay2005 <gupadhyay2005@gmail.com>
2026-06-27 00:37:15 +0530

    Update PointDemo.java

--- Week_4/OOPS/PointDemo.java
+++ Week_4/OOPS/PointDemo.java
@@ -1,1 +1,24 @@
+class Point {
 
+    int x;
+    int y;
+
+    void setPoint(int a, int b) {
+        x = a;
+        y = b;
+    }
+
+    void showPoint() {
+        System.out.println("Point = (" + x + ", " + y + ")");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Point p = new Point();
+
+        p.setPoint(10, 20);
+        p.showPoint();
+    }
+}
