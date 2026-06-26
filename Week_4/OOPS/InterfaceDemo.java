e78874575d853eb83b5081a6eae629fbbf0bbd3f
gupadhyay2005 <gupadhyay2005@gmail.com>
2026-06-27 00:36:48 +0530

    Update InterfaceDemo.java

--- Week_4/OOPS/InterfaceDemo.java
+++ Week_4/OOPS/InterfaceDemo.java
@@ -1,1 +1,19 @@
+interface Test {
+    void square(int n);
+}
 
+class Arithmetic implements Test {
+
+    public void square(int n) {
+        System.out.println("Square = " + (n * n));
+    }
+}
+
+public class ToTestInt {
+    public static void main(String[] args) {
+
+        Arithmetic obj = new Arithmetic();
+
+        obj.square(5);
+    }
+}
