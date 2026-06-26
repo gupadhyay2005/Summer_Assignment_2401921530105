9a11bf61565ecea2d37a927b927af7dad4d77910
gupadhyay2005 <gupadhyay2005@gmail.com>
2026-06-26 22:22:49 +0530

    Update Symmetric_Tree.java

--- Week_4/Day_5/Symmetric_Tree.java
+++ Week_4/Day_5/Symmetric_Tree.java
@@ -1,1 +1,34 @@
+class Solution {
 
+    public boolean isSymmetric(TreeNode root) {
+
+        if (root == null) {
+            return true;
+        }
+
+        return solve(root.left, root.right);
+    }
+
+    public boolean solve(TreeNode left, TreeNode right) {
+
+        if (left == null && right == null) {
+            return true;
+        }
+
+        if (left == null || right == null) {
+            return false;
+        }
+
+        if (left.val != right.val) {
+            return false;
+        }
+
+     boolean x =solve(left.left, right.right);
+     boolean y = solve(left.right, right.left);
+     if(x && y){
+        return true;
+     }else{
+        return false;
+     }
+    }
+}
